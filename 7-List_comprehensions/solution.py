b6c1b65cf232fdc166261044afe8e91a4c0a1724
vonThule <alexpopp82@gmail.com>
2018-05-13 20:48:16 +0200

    Added solution to the seventh problem.

---
+++ 7-List_comprehensions/solution.py
@@ -0,0 +1,7 @@
+# Input list
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print("List a: {}".format(a))
+
+# Output a list with only the even elements from input list
+print("[Result]\nResulting list of even elements from list a: {}".format([n for n in a if n % 2 == 0]))
+
