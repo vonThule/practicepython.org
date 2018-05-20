2a3fa1f3b920e032d7b3f6a64eb07326f25ffc8b
vonThule <alexpopp82@gmail.com>
2018-05-20 23:12:23 +0200

    Refactored first problem and added comments and header.

--- 1-Character_Input/solution.py
+++ 1-Character_Input/solution.py
@@ -1,57 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
-Problem #1, from: http://www.practicepython.org
-Create a program that asks the user to enter their name and their age.
-Print out a message addressed to them that tells them the year that
-they will turn 100 years old.
-Extras:
-		- Add on to the previous program by asking the user
-		for another number and printing out that many copies
-		of the previous message.
-		- Print out that many copies of the previous message
-		on separte lines.
+		File name: solution.py
+		Author: Alexander Popp
+		Date created: 5/13/2018
+		Date last modified: 5/20/2018
+		Python version: 3.4.6
+		Description:
+				Problem #1: Character Input from http://www.practicepython.org
+				For detailed description of the problem see the accompanying
+				file named 'problem.txt' or refer to the aformentioned website.
 """
 
 import datetime
 
 
-# Get user name
-name = input("Enter your name: ")
-while len(name) == 0:
-		print("You did not enter a name.\nPlease try again...")
-		name = input("Enter your name: ")
+# Get user name and check for valid input
+def get_user_name():
+		while True:
+				name = input("Enter your name: ")
+				# Name may not be of length 0
+				if len(name) == 0:
+						print("You did not enter a name.\nPlease try again...")
+				# Name must contain only letters and spaces
+				elif not all(char.isalpha() or char.isspace() for char in name):
+						print("You may only use letters and spaces.\nPlease try again...")
+				else:
+						break
+		return name.title()
 
-# Get user age
-while True:
-		try:
-				age = int(input("Enter your age: "))
-		except ValueError:
-				print("Fail! You did not enter correct age value.")
-				print("Please try again...")
-		else:
-				break
 
-# Get number of repeats for message
-# If out of scope, ask for input again
-while True:
-		try:
-				repeats = int(input("Enter a number from 1 to 20: "))
-				while repeats <= 0 or repeats > 20:
-						print("Your number is out of scope.\nPlease try again...")
+# Get user age and check for valid input
+def get_user_age(name):
+		while True:
+				try:
+						age = int(input("Hello {}, how old are you? ".format(name)))
+				# If entered age is not a number, catch exception and inform user
+				except ValueError:
+						print("You did not enter a number.\nPlease try again...")
+				else:
+						break
+		return age
+
+
+# Get number of repeats for message, if out of scope, ask for input again
+def get_number_repeats(name):
+		while True:
+				try:
+						print("How many times would you like the message displayed {}?".format(name))
 						repeats = int(input("Enter a number from 1 to 20: "))
-		except ValueError:
-				print("Fail! You did not enter a number.")
-				print("Please try again...")
-		else:
-				break
+						# If entered value is not within set range, notify user
+						if repeats <= 0 or repeats > 20:
+								print("Your number is out of scope.\nPlease try again...")
+						else:
+								break
+				# If entered value is not a number, catch exception and inform user
+				except ValueError:
+						print("You did not enter a number.\nPlease try again...")
+		return repeats
 
-# Calculate difference between users age and 100
-years_to_hundred = 100 - age
-# Get current year
-current_year = datetime.datetime.now().year
-# Calculate result
-result = current_year + years_to_hundred
 
-# Display result n number of times
-for n in range(0, repeats):
-		print("Hello, {}! You will turn 100 in the year {}.".format(name, result))
+# Calculate year to turn 100
+def get_hundredth_year(age):
+		current_year = datetime.datetime.now().year
+		return current_year + 100 - age
+
+
+# Main function
+def main():
+		user_name = get_user_name()
+		user_age = get_user_age(user_name)
+		message_repeats = get_number_repeats(user_name)
+		hundredth_year = get_hundredth_year(user_age)
+
+		# Display result n number of times
+		for n in range(0, message_repeats):
+				print("{}, you will turn 100 in the year {}.".format(user_name, hundredth_year))
+
+
+# Run program
+if __name__ == "__main__":
+		main()
 
