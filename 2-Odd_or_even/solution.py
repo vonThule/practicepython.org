2bcf36bc0aceabc8f20d982fa8dcadb4ec286347
vonThule <alexpopp82@gmail.com>
2018-05-21 19:08:39 +0200

    Refactored second problem and added comments and header.

--- 2-Odd_or_even/solution.py
+++ 2-Odd_or_even/solution.py
@@ -1,59 +1,68 @@
-"""
-Problem #2, from http://www.practicepython.org
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-Ask the user for a number. Depending on whether the number is even
-or odd, print out an appropriate message to the user.
-Extras:
-		- If the number is a multiple of 4, print out
-		a different message.
-		- Ask the user for two numbers: one number to
-		check (call it num) and one number to divide
-		by (check). If check divides evenly into num,
-		tell that to the user. If not, print a different
-		appropriate message.
 """
+		File name: solution.py
+		Author: Alexander Popp
+		Date created: 5/13/2018
+		Date last modified: 5/21/2018
+		Python version: 3.4.6
+		Description:
+				Problem #2: Odd or Even from http://www.practicepython.org
+				For detailed description of the problem see the accompanying
+				file named 'problem.txt' or refer to the aformentioned website.
+"""
+
 
 # Get user input and make shure input is a valid number
-while True:
-		try:
-				number = int(input("Enter a number: "))
-		except ValueError:
-				print("You did not enter a number.\nPlease try again...")
+def get_user_input(numeration):
+		while True:
+				try:
+						number = int(input("Enter {} number: ".format(numeration)))
+				# If a number is not entered, catch exception and inform user
+				except ValueError:
+						print("You did not enter a number.\nPlease try again...")
+				else:
+						break
+		return number
+
+
+# Check if number is even or odd and output the information
+def even_odd(number):
+		if number % 2 == 0:
+				print("Your number is even!")
 		else:
-				break
+				print("Your number is odd!")
 
-# Check if number is even or odd
-if number % 2 == 0:
-		print("Your number is even!")
-else:
-		print("Your number is odd!")
 
-# Check if number is a multiple of 4
-if number % 4 == 0:
-		print("Your number is a multiple of 4!")
+# Check if number is a multiple of 4 and output the information if so
+def multiple_of_four(number):
+		if number % 4 == 0:
+				print("Your number is a multiple of 4!")
 
-print("\n\n")
-# Get two user inputs and make shure they are valid numbers
-while True:
-		try:
-				num = int(input("Enter first number: "))
-		except ValueError:
-				print("You did not enter a number.\nPlease try again...")
+
+# Check if one number divides into another evenly and output the information
+def number_divide_into_another(num, check):
+		if num % check == 0:
+				print("Number {} divides evenly by {}.".format(num, check))
 		else:
-				break
+				print("Number {} does not divide evenly by {}.".format(num, check))
 
-while True:
-		try:
-				check = int(input("Enter second number: "))
-		except ValueError:
-				print("You did not enter a number.\nPlease try again...")
-		else:
-				break
 
-# Check first mod second and display message
-if num % check == 0:
-		print("Number {} divides evenly by {}.".format(num, check))
-else:
-		print("Number {} does not divide evenly by {}.".format(num, check))
+# Main function
+def main():
+		print("\n1. Check if number is even or odd or a multiple of 4...")
+		number = get_user_input("a")
+		even_odd(number)
+		multiple_of_four(number)
+		
+		print("\n2. Check if one number divides into another...")
+		num = get_user_input("first")
+		check = get_user_input("second")
+		number_divide_into_another(num, check)
 
+
+# Run program
+if __name__ == "__main__":
+		main()
 
