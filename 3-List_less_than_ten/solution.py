483d1a43d1723cf4a9d967829812058881941122
vonThule <alexpopp82@gmail.com>
2018-05-22 11:17:49 +0200

    Refactored third problem and added comments and header.

--- 3-List_less_than_ten/solution.py
+++ 3-List_less_than_ten/solution.py
@@ -1,53 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
-Problem no 3, from htttp://www.practicepython.org
-Take a list, for example: a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-and write a program that prints out all the elements of the list that
-are less than 5.
-Extras:
-		- Instead of printing the elements one by one, make a new
-		list that has all the elements less than 5 from this list
-		in it and print out this new list.
-		- Write this in one line of Python.
-		- Ask the user for a number and return a list that contains
-		only elements from the original list, a, that are smaller
-		than that number given by the user.
+		File name: solution.py
+		Author: Alexander Popp
+		Date created: 5/13/2018
+		Date last modified: 5/22/2018
+		Python version: 3.4.6
+		Description:
+				Problem #3: List Less Than Ten from http://www.practicepython.org
+				For detailed description of the problem see the accompanying
+				file named 'problem.txt' or refer to the aformentioned website.
 """
 
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-print("Original list:", a)
-
-# 1. Print out all elements less than 5
-print("Elements that are less than 5:")
-for element in a:
-		if element < 5:
-				print(element)
+# Print out all elements less than 5
+def print_less_than_five(a):
+		print("Elements that are less than 5:")
+		for element in a:
+				if element < 5:
+						print(element)
 
 
-# 2. Make a new list of elements less than 5 and print it
-new_list = []
-for element in a:
-		if element < 5:
-				new_list.append(element)
+# Make a new list of elements less than 5
+def create_list(a):
+		new_list = []
+		for element in a:
+				if element < 5:
+						new_list.append(element)
+		return new_list
 
-print("List of elements less than 5:", new_list)
-
-# 3. Make a new list of elements less than 5 and print it(one liner)
-print("List of elements less than 5(oneliner):", [element for element in a if element < 5])
-
-print("\n")
-# 4. Ask user for input and print a list that contains elements smaller
-# than the users number
 
 # Get user input
-while True:
-		try:
-				number = int(input("Enter a number: "))
-		except ValueError:
-				print("You did not enter a number.\nPlease try again...")
-		else:
-				break
+def get_input():
+		while True:
+				try:
+						number = int(input("Enter a number: "))
+				# If input is not a number catch exception and inform user
+				except ValueError:
+						print("You did not enter a number.\nPlease try again...")
+				else:
+						break
+		return number
 
-# Check condition and print
-print("List of elements less than number {}: {}".format(number, [element for element in a if element < number]))
+
+# Main function
+def main():
+		# Define a list and print it
+		a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+		print("Original list:", a)
+		
+		# Print out elements of the list that are less than 5
+		print_less_than_five(a)
+		
+		# Make a new list of elements less than 5 and print it
+		new_list = create_list(a)
+		print("List of elements less than 5:", new_list)
+
+		# Make a new list of elements less than 5 and print it(one liner)
+		print("List of elements less than 5(oneliner):", [element for element in a if element < 5])
+
+		# Ask user for input and print a list that contains elements smaller
+		# than the users number
+		user_input = get_input()
+		print("List of elements less than number {}: {}".format(user_input, [element for element in a if element < user_input]))
+
+
+# Run program
+if __name__ == "__main__":
+		main()
 
