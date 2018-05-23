e9aea08932bdac21a272651236b10c2f855e4c5c
vonThule <alexpopp82@gmail.com>
2018-05-23 15:55:04 +0200

    Refactored problem nine and added headers and comments.

--- 9-Guessing_game_one/solution.py
+++ 9-Guessing_game_one/solution.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+		File name: solution.py
+		Author: Alexander Popp
+		Date created: 5/13/2018
+		Date last modified: 5/23/2018
+		Python version: 3.4.6
+		Description:
+				Problem #9: Guessing Game One from http://www.practicepython.org
+				For detailed description of the problem see the accompanying
+				file named 'problem.txt' or refer to the aformentioned website.
+"""
+
+
 from random import randint
 
 
@@ -9,40 +25,68 @@ def generate_random():
 # Get user input function
 def get_input():
 		while True:
-				user_input = input("Guess a number from 1 to 9 or type 'exit' to quit: ")
+				user_input = input("\nGuess a number from 1 to 9 or type 'exit' to quit: ")
 				if user_input == "exit":
 						break
+				# Check if user_input is a number between 1 and 9
 				try:
 						if int(user_input) > 0 and int(user_input) < 10:
 								break
+						else:
+								print("You must enter a number between 1 and 9.\nPlease try again...")
+				# If user_input is not a number catch exception and inform user
 				except ValueError:
 						print("You did not enter a proper choice.\nPlease try again...")
 		return user_input
 
 
-# Main function
-def main():
-		counter = 0
+# Ask for another game
+def play_again():
 		while True:
-				random_number = generate_random()
-				print("Debug:", random_number)
+				again = input("Play again? (y/n) ")
+				# Check if answer is valid
+				if again.lower() != "y" and again.lower() != "n":
+						print("Wrong input. You must enter either 'y' or 'n'.\nPlease try again...")
+				else:
+						break
+		if again.lower() == "n":
+				return False
+		return True
+
+
+# Game function
+def game(random_number):
+		# Keep track of number of guesses
+		counter = 1
+		while True:
 				user_choice = get_input()
+				# If user types 'exit' break game
 				if user_choice == "exit":
 						break
-				if int(user_choice) == random_number:
-						print("You guessed correct. The number is: {}.".format(random_number))
-						counter += 1
-				else:
-						print("You did not guess correct. The number is: {}.".format(random_number))
-				again = input("Play again? (y/n) >> ")
-				while True:
-						if again.lower() != "y" and again.lower() != "n":
-								print("Wrong choice.\nPlease try again...")
-						else:
-								break
-				if again.lower() == "n":
+				# Check guesses and display proper information
+				elif int(user_choice) == random_number:
+						print("You guessed correct. The number is {} and it took you {} tries.".format(random_number, counter))
 						break
-		print("Goodbye. Your had {} correct guesses!".format(counter))
+				elif int(user_choice) < random_number:
+						print("You guessed lower. Guess again...")
+				else:
+						print("You guessed higer. Guess again...")
+				# If number was not guessed correctly, increase count of tries
+				counter += 1
+
+
+# Main function
+def main():
+		print("\n=========================================")
+		print("||            GUESSING GAME            ||")
+		print("=========================================")
+		while True:
+				random_number = generate_random()
+				game(random_number)
+				# If player does not want another game, exit
+				if not play_again():
+						break
+		print("Goodbye.")
 
 
 # Run program
