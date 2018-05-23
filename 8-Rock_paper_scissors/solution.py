b3599069c69fb19472d4077ad37816d53bff1392
vonThule <alexpopp82@gmail.com>
2018-05-23 15:19:12 +0200

    Refactored problem eight and added headers and comments.

--- 8-Rock_paper_scissors/solution.py
+++ 8-Rock_paper_scissors/solution.py
@@ -1,46 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+		File name: solution.py
+		Author: Alexander Popp
+		Date created: 5/13/2018
+		Date last modified: 5/23/2018
+		Python version: 3.4.6
+		Description:
+				Problem #8: Rock Paper Scissors from http://www.practicepython.org
+				For detailed description of the problem see the accompanying
+				file named 'problem.txt' or refer to the aformentioned website.
+"""
+
+
 # Get user choice function
 def player_choice(player):
-		print("[{}]:".format(player))
+		print("\n[{}]:".format(player))
 		print("1. Rock\n2. Paper\n3. Scissors\n4. Quit")
 		while True:
 				try:
 						player_input = int(input("Your choice >>> "))
+						# player_input must be a number between 1 and 4
+						if player_input >= 1 and player_input <= 4:
+								break
+						# If not, inform the player and ask for another input
+						else:
+								print("You must enter a number between 1 and 4.\nPlease try again...")
+				# If player_input not an integer, catch exception and inform player
 				except ValueError:
-						print("You didn't input the proper choice.\nPlease try again...")
-				else:
-						break
+						print("You did not enter a number.\nPlease try again...")
 		return player_input
 
 
-# Check result function
+# Check result of game and display the result string
 def check_result(pl1choice, pl2choice):
-		if pl1choice == 1 and pl2choice == 2:
-				return 2
-		elif pl1choice == 1 and pl2choice == 3:
-				return 1
-		elif pl1choice == 2 and pl2choice == 1:
-				return 1
-		elif pl1choice == 2 and pl2choice == 3:
-				return 2
-		elif pl1choice == 3 and pl2choice == 1:
-				return 2
-		elif pl1choice ==3 and pl2choice == 2:
-				return 1
+		if (pl1choice == 1 and pl2choice == 2 or
+		    pl1choice == 2 and pl2choice == 3 or
+		    pl1choice == 3 and pl2choice == 1):
+				print("\nPlayer 2 wins!\n")
+		elif (pl1choice == 1 and pl2choice == 3 or
+		      pl1choice == 2 and pl2choice == 1 or
+			  pl1choice == 3 and pl2choice == 2):
+				print("\nPlayer 1 wins!\n")
 		else:
-				return 0
+				print("\nIt's a tie!\n")
 
 
-# Display result function
-def display_result(result):
-		if result == 1:
-				print("Player 1 wins!")
-		elif result == 2:
-				print("Player 2 wins!")
-		else:
-				print("It's a draw!")
+# Ask if players want another game and return the answer
+def another_game():
 		while True:
 				new = input("New game? (y/n) >> ")
-				if new != "y" and new != "n":
+				# If input is not 'y' or 'n' inform players and ask for another input
+				if new.lower() != "y" and new.lower() != "n":
 						print("You didn't input the right choice.\nPlease try again...")
 				else:
 						break
@@ -49,6 +61,9 @@ def display_result(result):
 
 # Main function
 def main():
+		print("\n=========================================")
+		print("||        ROCK, PAPER, SCISSORS        ||")
+		print("=========================================")
 		while True:
 				pl1 = player_choice("Player 1")
 				if pl1 == 4:
@@ -57,8 +72,8 @@ def main():
 				if pl2 == 4:
 						break
 				result = check_result(pl1, pl2)
-				end = display_result(result)
-				if end == "n":
+				again = another_game()
+				if again == "n":
 						break
 
 
