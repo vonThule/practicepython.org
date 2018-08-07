d82d9c2b9e58dc89e4f2fc239d7443612185a645
Alexander Popp <alexpopp82@gmail.com>
2018-08-07 15:50:39 +0200

    Added headers and corrdcted indentation for problem 36.

--- 36-Birthday_plots/solution.py
+++ 36-Birthday_plots/solution.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    File name: solution.py
+    Author: Alexander Popp
+    Date created: 5/20/2018
+    Date last modified: 8/7/2018
+    Python version: 3.6.5
+    Description:
+        Problem #36: Birthday Plots from http://www.practicepython.org
+        For detailed description of the problem see the accompanying
+        file named 'problem.txt' or refer to the aformentioned website.
+"""
+
+
 import json
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import Range1d
@@ -5,41 +21,41 @@ from bokeh.models import Range1d
 
 # Read file
 def read_data_file(filename):
-		with open(filename, "r") as f:
-				data = json.load(f)
-		return data
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
 
 
 # Create list of values:
 def create_list(data):
-		months = ["January", "February", "March", "April", "May", "June",
-				  "July", "August", "September", "October", "November", "December"]
-		lst = []
-		for month in months:
-				if month not in data:
-						lst.append(0)
-				else:
-						lst.append(data[month])
-		return lst
+    months = ["January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December"]
+    lst = []
+    for month in months:
+        if month not in data:
+            lst.append(0)
+        else:
+            lst.append(data[month])
+    return lst
 
 
 # Main function
 def main():
-		months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
-				  "Aug", "Sep", "Oct", "Nov", "Dec"]
-		stats = read_data_file("stats.json")
-		vals = create_list(stats)
-		output_file("plot.html")
-
-		p = figure(plot_width=400, plot_height=400)
-		p.x_range = Range1d(0,13)
-		p.vbar(x=[1,2,3,4,5,6,7,8,9,10,11,12], width=0.5, bottom=0,
-		       top=vals, color="firebrick")
-
-		show(p)
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+              "Aug", "Sep", "Oct", "Nov", "Dec"]
+    stats = read_data_file("stats.json")
+    vals = create_list(stats)
+    output_file("plot.html")
+    
+    p = figure(plot_width=400, plot_height=400)
+    p.x_range = Range1d(0,13)
+    p.vbar(x=[1,2,3,4,5,6,7,8,9,10,11,12], width=0.5, bottom=0,
+    top=vals, color="firebrick")
+    
+    show(p)
 
 
 # Run program
 if __name__ == "__main__":
-		main()
+    main()
 
